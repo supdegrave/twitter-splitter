cd67bb0ca5a61d6f9da813125603f7e64cc1450c
Stuart Updegrave <stuart@updegrave.com>
2014-02-04 11:23:47 +0100

    only show delimiters if message length < limit

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ $( function() {
   }); 
 });
 
-
 function tweetify() {
   var message = input.val();
   var delimiterLength = Settings.DelimiterStyle.length + 1; // add 1 for whitespace 
@@ -32,27 +31,34 @@ function tweetify() {
   var len = tweetLength - delimiterLength;
   var messages = [];
   var newline, sentence, slicePoint, retval;
-  
-  while (message.length != 0) {
-    newline = message.lastIndexOf('\n', len);
-    sentence = message.lastIndexOf('.', len);
-    
-    slicePoint = (-1 !== newline) 
-      ? newline : ("Yes" === Settings.BreakOnSentenceEnd && -1 !== sentence) 
-        ? sentence + 1 : (message.length > len) 
-          ? message.lastIndexOf(' ', len) : len;
 
-    messages.push(message.slice(0, slicePoint));
-    message = message.slice(slicePoint < len ? slicePoint + 1 : len).trim();
+  if (message.length > len) {
+    while (message.length != 0) {
+      newline = message.lastIndexOf('\n', len);
+      sentence = message.lastIndexOf('. ', len);
+
+      slicePoint = (-1 !== newline) 
+        ? newline : ("Yes" === Settings.BreakOnSentenceEnd && -1 !== sentence) 
+          ? sentence + 1 : (message.length > len) 
+            ? message.lastIndexOf(' ', len) : len;
+
+      messages.push(message.slice(0, slicePoint));
+      message = message.slice(slicePoint < len ? slicePoint + 1 : len).trim();
+    }
+
+    messages = messages.filter( function(elem) {
+      return elem.trim().length > 0;
+    });
   }
-    
-  messages = messages.filter( function(elem) {
-    return elem.trim().length > 0;
-  });
+  else {
+    messages.push(message);
+  }
   
-  return $.map( messages, function(value, index) {
-    var delimiter = Settings.DelimiterStyle.replace('x', index + 1).replace('y', messages.length);
-    
+  return (1 === messages.length) ? messages[0] : $.map( messages, function(value, index) {
+    var delimiter = Settings.DelimiterStyle
+                    .replace('x', index + 1)
+                    .replace('y', messages.length);
+                    
     return "End" == Settings.DelimiterLocation 
       ? value.trim() + " " + delimiter
         : delimiter + " " + value.trim()
---
+++ helpers.js
@@ -0,0 +1,5 @@
+function log() {
+  $.each(arguments, function(k,v) {
+    console.log(v);    
+  });
+}
