abf172a751473cd2352e65f2b06cabd7e15cf0d0
Stuart Updegrave <stuart@updegrave.com>
2014-02-04 23:39:09 +0100

    remove masks from input[value]

--- app.js
+++ app.js
@@ -1,36 +1,54 @@
-var Settings, 
+var Position = { Any: 'any', Start: 'start', End: 'end' }, 
+  
+    Delimiters = {
+      XYbrackets : { value: '[x/y]', position: Position.Any },
+      XYplain    : { value: 'x/y', position: Position.Any },
+      Xcolon     : { value: 'x:', position: Position.Start },
+      Xonly      : { value: 'x', position: Position.Any }
+    },
+    
+    Settings = {    
+      DelimiterStyle     : Delimiters.XYbrackets.value,
+      DelimiterLocation  : Position.End,
+      BreakOnSentenceEnd : true
+    }, 
+    
     input;
 
 $( function() {
-  Settings = {    
-    DelimiterStyle: '[x/y]',
-    DelimiterLocation: 'End',
-    BreakOnSentenceEnd: 'Yes'
-  };
-
   // bind textarea content change handler
-  input = $('#input')
-  input.bind('input propertychange', function() {
-    $('#output').val( tweetify() );
+  input = $('#input');
+  input.bind('input propertychange', function(e) {
+    tweetify();
   });
   
   // bind radio group change handlers 
   $.each( ['DelimiterStyle', 'DelimiterLocation', 'BreakOnSentenceEnd'], function (key, value) {
-    var radios = $('input:radio[name=' + value +']');
-    radios.change( function() { 
-      Settings[value] = radios.filter(':checked').val();
-      $('#output').val( tweetify() );
+    $('input:radio[name=' + value +']').change( function() { 
+      switch(value) {
+        case "DelimiterStyle":
+          Settings.DelimiterStyle = Delimiters[this.id].value;
+          break;
+        case "DelimiterLocation":
+          Settings.DelimiterLocation = this.id.slice(this.id.indexOf('-') + 1);
+          break;
+        case "BreakOnSentenceEnd":
+          Settings.BreakOnSentenceEnd = !!this.id.match("yes")
+          break;
+      }
+      
+      tweetify();
     });
   }); 
 });
 
 function tweetify() {
-  var message = input.val();
-  var delimiterLength = Settings.DelimiterStyle.length + 1; // add 1 for whitespace 
-  var tweetLength = 140; // twitter length limit 
-  var len = tweetLength - delimiterLength;
-  var messages = [];
-  var newline, sentence, slicePoint, retval;
+  var message         = input.val(),
+      messages        = [],
+      tweetLength     = 140, // twitter length limit 
+      delimiterLength = Settings.DelimiterStyle.length + 1, // add 1 for whitespace 
+      len             = tweetLength - delimiterLength,
+      newline, sentence, slicePoint, retval;
 
   if (message.length > len) {
     while (message.length != 0) {
@@ -38,7 +56,7 @@ function tweetify() {
       sentence = message.lastIndexOf('. ', len);
 
       slicePoint = (-1 !== newline) 
-        ? newline : ("Yes" === Settings.BreakOnSentenceEnd && -1 !== sentence) 
+        ? newline : (Settings.BreakOnSentenceEnd && -1 !== sentence) 
           ? sentence + 1 : (message.length > len) 
             ? message.lastIndexOf(' ', len) : len;
 
@@ -54,13 +72,17 @@ function tweetify() {
     messages.push(message);
   }
   
-  return (1 === messages.length) ? messages[0] : $.map( messages, function(value, index) {
+  retval = (1 === messages.length) 
+  ? messages[0] 
+  : $.map( messages, function(value, index) {
     var delimiter = Settings.DelimiterStyle
                     .replace('x', index + 1)
                     .replace('y', messages.length);
                     
-    return "End" == Settings.DelimiterLocation 
+    return Position.End == Settings.DelimiterLocation 
       ? value.trim() + " " + delimiter
         : delimiter + " " + value.trim()
   }).join('\n');
+  
+  $('#output').val(retval);
 }
